46536f8602e089651e7144e9e81d6b5ac5779c9c
Samuel Pedro <samuelpedro.ctt@gmail.com>
2025-06-21 13:51:21 -0300

    resolvendo ordem

--- database/seeds/SiteContatoSeeder.php
+++ database/seeds/SiteContatoSeeder.php
@@ -13,15 +13,15 @@ class SiteContatoSeeder extends Seeder
     public function run()
     {
 
-        /*
+
         $contato = new SiteContato();
         $contato->nome = 'Sistema SG';
         $contato->telefone = '(11) 99999-8888';
         $contato->email = 'contato@sg.com.br';
-        $contato->motivo_contato = 1;
+        $contato->motivo_contato_id = 1;
         $contato->mensagem = 'Seja bem-vindo ao sistema Super Gestão';
         $contato->save();
-        */
+
 
         factory(SiteContato::class, 100)->create();
     }
